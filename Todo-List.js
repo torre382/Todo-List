85e49a2660a6c8b4a5885ea264f4464a0753ec32
Roy Torres <roytorres@Roys-MacBook-Pro.local>
2020-11-28 21:08:13 -0600

    added tasks

--- Todo-List.js
+++ Todo-List.js
@@ -1,31 +1,58 @@
-let div = document.querySelector("TaskList");
-let newnode = document.createElement("div")
+const InputTask = document.getElementById('InputTask');
+
+InputTask.addEventListener('input', function (event) {
+    if (InputTask.validity.valueMissing) {
+        InputTask.setCustomValidity('Please input a number.');
+    }
+    else {
+        InputTask.setCustomValidity('');
+    }
+});
 
 function myResponse() {
 
-let task = document.getElementById("InputTask");
+let task = document.getElementById("InputTask").value;
 let days = Number(document.getElementById("DaysToComplete").value);
 
-console.log(task);
+let TaskList = document.getElementById("TaskList");
+let newCol = document.createElement("div")
 
 
-    if (days < 7) {
-        newnode.textContent = "${task} \r\n ${days}";
-        newnode.setAttribute('class', 'alert alert-warning');
-        newnode.setAttribute('role', 'alert');
-        div.appendChild(newnode);  
+    if (days < 7 & days >= 3) {
+        newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-warning alert-dismissible fade show');
+        newCol.setAttribute('role', 'alert');
+        newCol.innerHTML = `${task}
+        <div class="row">
+            <div class="col">${days} day(s)</div>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`;
+        TaskList.appendChild(newCol);
     }
     else if (days < 3) {
-        newnode.textContent = "${task} \r\n ${days}";
-        newnode.setAttribute('class', 'alert alert-danger');
-        newnode.setAttribute('role', 'alert');
-        div.appendChild(newnode);
+        newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-danger alert-dismissible fade show');
+        newCol.setAttribute('role', 'alert');
+        newCol.innerHTML = `${task}
+        <div class="row">
+            <div class="col">${days} day(s)</div>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`;
+        TaskList.appendChild(newCol);
     }
     else {
-        newnode.textContent = "${task} \r\n ${days}";
-        newnode.setAttribute('class', 'alert alert-secondary');
-        newnode.setAttribute('role', 'alert');
-        div.appendChild(newnode);
+        newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-secondary alert-dismissible fade show');
+        newCol.setAttribute('role', 'alert');
+        newCol.innerHTML = `${task}
+        <div class="row">
+            <div class="col">${days} day(s)</div>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+            </button>
+        </div>`;
+        TaskList.appendChild(newCol);
     }
 
 }
