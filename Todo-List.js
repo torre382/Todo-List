097811cd94de1c7a1b2282b3bc0571405d58a57f
Roy Torres <roytorres@Roys-MacBook-Pro.local>
2020-12-02 10:35:22 -0600

    added form validation and fixed bugs

--- Todo-List.js
+++ Todo-List.js
@@ -1,58 +1,82 @@
-const InputTask = document.getElementById('InputTask');
+function myValidation() {
+    let task = document.getElementById("InputTask");
+    let days = document.getElementById("DaysToComplete"); 
+    let form = document.getElementById("myForm");
+    form.addEventListener('click', function(event){
+        if (task.validity.valueMissing) { 
+            task.setCustomValidity('Please fill out this field.');
+            task.reportValidity();
+            event.preventDefault();
+            } 
+        else if (days.validity.valueMissing) {
+            days.setCustomValidity('Please enter a number.');
+            days.reportValidity();
+            event.preventDefault();
+        }
+        else if (days.value < 1) {
+            days.setCustomValidity('Please select a value that is no less than 1.');
+            days.reportValidity();
+            event.preventDefault();
+        }
+        else if (days.validity.typeMismatch) {
+            days.setCustomValidity('Please enter a number.');
+            days.reportValidity();
+            event.preventDefault();
+        }
+        else { 
+            task.setCustomValidity('');
+            days.setCustomValidity('');
+        } 
+    });
+}
 
-InputTask.addEventListener('input', function (event) {
-    if (InputTask.validity.valueMissing) {
-        InputTask.setCustomValidity('Please input a number.');
+function myTasks() {
+    let task = document.getElementById("InputTask").value;
+    let days = Number(document.getElementById("DaysToComplete").value); 
+
+    if (task == "") {
+        return false;
     }
     else {
-        InputTask.setCustomValidity('');
+        let TaskList = document.getElementById("TaskList");
+        let newCol = document.createElement("div")
+
+        if (days < 7 & days >= 3) {
+            newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-warning alert-dismissible fade show');
+            newCol.setAttribute('role', 'alert');
+            newCol.innerHTML = `${task}
+            <div class="row">
+                <div class="col">${days} day(s)</div>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`;
+            TaskList.appendChild(newCol);
+        }
+        else if (days < 3 & days > 0) {
+            newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-danger alert-dismissible fade show');
+            newCol.setAttribute('role', 'alert');
+            newCol.innerHTML = `${task}
+            <div class="row">
+                <div class="col">${days} day(s)</div>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`;
+            TaskList.appendChild(newCol);
+        }
+        else if (days >= 7) {
+            newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-secondary alert-dismissible fade show');
+            newCol.setAttribute('role', 'alert');
+            newCol.innerHTML = `${task}
+            <div class="row">
+                <div class="col">${days} day(s)</div>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>`;
+            TaskList.appendChild(newCol);
+        }
+        return true;
     }
-});
-
-function myResponse() {
-
-let task = document.getElementById("InputTask").value;
-let days = Number(document.getElementById("DaysToComplete").value);
-
-let TaskList = document.getElementById("TaskList");
-let newCol = document.createElement("div")
-
-
-    if (days < 7 & days >= 3) {
-        newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-warning alert-dismissible fade show');
-        newCol.setAttribute('role', 'alert');
-        newCol.innerHTML = `${task}
-        <div class="row">
-            <div class="col">${days} day(s)</div>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>`;
-        TaskList.appendChild(newCol);
-    }
-    else if (days < 3) {
-        newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-danger alert-dismissible fade show');
-        newCol.setAttribute('role', 'alert');
-        newCol.innerHTML = `${task}
-        <div class="row">
-            <div class="col">${days} day(s)</div>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>`;
-        TaskList.appendChild(newCol);
-    }
-    else {
-        newCol.setAttribute('class', 'col-xl-fluid m-2 alert alert-secondary alert-dismissible fade show');
-        newCol.setAttribute('role', 'alert');
-        newCol.innerHTML = `${task}
-        <div class="row">
-            <div class="col">${days} day(s)</div>
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-            </button>
-        </div>`;
-        TaskList.appendChild(newCol);
-    }
-
 }
